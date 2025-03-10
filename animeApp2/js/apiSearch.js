66c200c8c75b138ad596475dbdcf411cdd1704ef
IgnacioGB00 <ignaciogb900@gmail.com>
2025-03-10 11:41:42 -0600

    Update apiSearch.js

--- animeApp2/js/apiSearch.js
+++ animeApp2/js/apiSearch.js
@@ -10,6 +10,11 @@ urlanime.searchname = function (data) {
         let imagen = document.createElement('img');
         imagen.src = data.images.jpg.image_url;
         imagen.classList.add('lazyload');
+          imagen.src = data.images.jpg.image_url;
+         imagen.classList.add('lazyload');
+         imagen.loading = 'lazy';
+         imagen.srcset = `${data.images.jpg.image_url}?w=400 400w, ${data.images.jpg.image_url}?w=800 800w, ${data.images.jpg.image_url}?w=1200 1200w`; // Añadir diferentes tamaños
+         imagen.sizes = "(max-width: 600px) 400px, (max-width: 1200px) 800px, 1200px"; // Define cómo se deben mostrar las imágenes
         imagen.alt = data.title;
         let titulos = document.createElement('h3');
         titulos.classList.add('titulos', 'tituloH3', 'titleBlue');
