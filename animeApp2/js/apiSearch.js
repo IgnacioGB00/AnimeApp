f658063c2d5f07fcca918206fc0d53c61582a616
IgnacioGB00 <ignaciogb900@gmail.com>
2025-03-10 12:44:40 -0600

    Update apiSearch.js

--- animeApp2/js/apiSearch.js
+++ animeApp2/js/apiSearch.js
@@ -9,6 +9,8 @@ urlanime.searchname = function (data) {
         cardTitulos.classList.add('conenedorTitulo', 'cardTitulo');
         let imagen = document.createElement('img');
         imagen.src = data.images.jpg.image_url;
+        imagen.srcset = `${data.images.jpg.image_url}?w=400 400w, ${data.images.jpg.image_url}?w=800 800w, ${data.images.jpg.image_url}?w=1200 1200w`; // Añadir diferentes tamaños
+        imagen.sizes = "(max-width: 600px) 400px, (max-width: 1200px) 800px, 1200px"; // Define cómo se deben mostrar las imágenes
         imagen.alt = data.title;
         let titulos = document.createElement('h3');
         titulos.classList.add('titulos', 'tituloH3', 'titleBlue');
