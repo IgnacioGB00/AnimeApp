92f7b3776f3c01443b6e36f32c93b1e5cc02ad0f
IgnacioGB00 <ignaciogb900@gmail.com>
2025-03-10 12:27:00 -0600

    Update filter.js

--- animeApp2/js/filter.js
+++ animeApp2/js/filter.js
@@ -13,6 +13,8 @@ urlanime.animeFilter = function (data) {
          imagen.src = data.images.jpg.image_url;
          imagen.classList.add('lazyload');
          imagen.loading = 'lazy';
+         imagen.width = 300;  // Ajusta según corresponda
+        imagen.height = 450
          imagen.srcset = `${data.images.jpg.image_url}?w=400 400w, ${data.images.jpg.image_url}?w=800 800w, ${data.images.jpg.image_url}?w=1200 1200w`; // Añadir diferentes tamaños
          imagen.sizes = "(max-width: 600px) 400px, (max-width: 1200px) 800px, 1200px"; // Define cómo se deben mostrar las imágenes
          imagen.alt = data.title;
